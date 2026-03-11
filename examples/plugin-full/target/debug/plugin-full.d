59578de7437c4688620e2791f6f9faf5288a3fe5
lasse <77295879+lassejlv@users.noreply.github.com>
2026-03-12 00:43:11 +0100

    Delete examples/plugin-full/target directory

--- examples/plugin-full/target/debug/plugin-full.d
+++
@@ -1,1 +0,0 @@
-/Users/lassevestergaard/Documents/dev/termy/examples/plugin-full/target/debug/plugin-full: /Users/lassevestergaard/Documents/dev/termy/crates/plugin_core/src/lib.rs /Users/lassevestergaard/Documents/dev/termy/crates/plugin_sdk/src/lib.rs /Users/lassevestergaard/Documents/dev/termy/examples/plugin-full/src/main.rs
